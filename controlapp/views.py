05de98c2874df9557a7668b865f6afb6e7745db7
SagarR.B <sagarrb1805@gmail.com>
2022-03-22 15:57:22 +0530

    created signup page and add signup fucntionality need to provide further functionality like duplication prevention password verification etc

--- controlapp/views.py
+++ controlapp/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
     return render(request, 'controlapp/index.html')
 
-def signup(request):
-    return render(request, 'controlapp/signup.html')
-
 def signin(request):
     return render(request, 'controlapp/signin.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+
+        messages.success(request, 'Your account has been successfuly created')
+        return redirect('signin')
+    return render(request, 'controlapp/signup.html')
+
 
 def signout(requst):
     pass
