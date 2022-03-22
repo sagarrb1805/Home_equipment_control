de69ca05147a21b9b88350f2931eb394390d3d16
SagarR.B <sagarrb1805@gmail.com>
2022-03-22 15:34:26 +0530

    created base form for signup

--- controlapp/views.py
+++ controlapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'controlapp/index.html')
+
+def signup(request):
+    return render(request, 'controlapp/signup.html')
+
+def signin(request):
+    return render(request, 'controlapp/signin.html')
+
+def signout(requst):
+    pass
