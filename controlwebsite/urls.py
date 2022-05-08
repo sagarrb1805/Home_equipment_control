ed08c5b2a5588b86d7b379c56ee60601a3f57ec7
SagarR.B <sagarrb1805@gmail.com>
2022-05-08 15:37:22 +0530

    added files for deploy

--- controlwebsite/urls.py
+++ controlwebsite/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('controlapp.urls')),
     path('', include('api.urls')),
+
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
